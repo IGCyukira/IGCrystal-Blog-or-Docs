5ec17edd56a3fc69a01dcd1e88acd9296927002d
IGCrystal <IGCrystal@outlook.jp>
2026-02-13 22:13:46 +0800

    fix: simplify sidebar configuration for '/learn/' section

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,17 +1,5 @@
 import type { ThemeSidebarMulti } from 'vuepress-theme-plume';
 
 export default {
-    '/learn/': [
-        {
-            text: '葵蕾謠',
-            collapsed: false, // VuePress 2.x 中的 `collapsable` 替换为 `collapsed`
-            items: [
-            '/learn/',  // README.md 对应路径
-            '/learn/1.md', 
-            '/learn/2.md',
-            '/learn/3.md',
-            '/learn/5.md',
-            ],
-        },
-    ],
+    '/learn/': 'auto',
 } as ThemeSidebarMulti;
