9bfcb0843e47fd19f8d7a6a586fe6d761d878ed8
IGCrystal <IGCrystal@outlook.jp>
2026-02-13 22:20:15 +0800

    fix: update sidebar configuration for '/learn/' section and modify title in documentation

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -1,5 +1,12 @@
 import type { ThemeSidebarMulti } from 'vuepress-theme-plume';
 
 export default {
-    '/learn/': 'auto',
+    '/learn/': [
+        {
+            text: '葵蕾謠',
+            collapsed: false,
+            items: 'auto',
+            prefix: '/learn/',
+        },
+    ],
 } as ThemeSidebarMulti;
